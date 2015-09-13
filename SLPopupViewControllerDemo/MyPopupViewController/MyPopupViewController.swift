a91601d50abf876df5e4979e07912d5354807cac
Nguyen Duc Hoang <sunlight3d@icloud.com>
2015-09-13 18:36:16 +0700

    This library is used for ios dev to display PopupViewController

---
+++ SLPopupViewControllerDemo/MyPopupViewController/MyPopupViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MyPopupViewController.swift
+//  SLPopupViewControllerDemo
+//
+//  Created by Nguyen Duc Hoang on 9/13/15.
+//  Copyright © 2015 Nguyen Duc Hoang. All rights reserved.
+//
+
+import UIKit
+
+protocol MyPopupViewControllerDelegate {
+    func pressOK(sender: MyPopupViewController)
+    func pressCancel(sender: MyPopupViewController)
+}
+class MyPopupViewController: UIViewController {
+    var delegate:MyPopupViewControllerDelegate?
+    @IBAction func btnOK(sender:UIButton) {
+        self.delegate?.pressOK(self)
+    }
+    
+    @IBAction func btnCancel(sender:UIButton) {
+        self.delegate?.pressCancel(self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.layer.cornerRadius = 12
+        self.view.layer.masksToBounds = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
